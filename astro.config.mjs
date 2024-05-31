2bdf561c986368cd885b4b432b2f308b0070b217
DebelxD <debelxd26@gmail.com>
2024-05-31 15:12:29 -0400

    one commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://animetaiyaki.netlify.app',
   integrations: [mdx(), sitemap()]
 });
