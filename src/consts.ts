b5d97e1e1112bfc1de88454d49f0a245daa75219
DebelxD <debelxd26@gmail.com>
2024-06-01 17:00:08 -0400

    Seo problem

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE  = '▷ AnimeTaiyaki' ;
-export const SITE_DESCRIPTION = 'Descargar ANIME por Mega y Mediafire ✅ Sub español ✅ HD ✅ 1 solo link ✅ Todos los capitulos en carpeta contenedora ✅ Descarga directa.';
+export const SITE_TITLE  = '▷ AnimeTaiyaki Descargar ✅ Novelas visuales por Mediafire' ;
+export const SITE_DESCRIPTION = 'Descargar ANIME por Mega y Mediafire ✅ Sub español ✅ HD ✅ 1 solo link ✅ Todos los capitulos en carpeta contenedora ✅ ';
